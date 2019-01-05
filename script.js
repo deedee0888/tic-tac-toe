2912a87daa7a8d722a4976b893f05a7ebffb0acd
deedee0888 <35111751+deedee0888@users.noreply.github.com>
2019-01-05 17:20:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var orgBord;
+var orgBoard;
 const firstPlayer = 'O';
 const secondPlayer = 'X';
 const winSet = [
@@ -12,28 +12,40 @@ const winSet = [
     [6,4,2]
 ]
 
+var currentMove = 1;
+var movesMade = 0;
+
+
 const cell = document.querySelectorAll('.cell');
+
+
 startGame();
 
 function startGame() {
     document.querySelector(".finish").style.display = "none";
-    orgBord = Array.from(Array(9).keys());
+    orgBoard = Array.from(Array(9).keys());
 
     for(var i = 0; i < cell.length; i++) {
         cell[i].innerText = '';
         cell[i].style.removeProperty('background-color');
-        cell[i].addEventListener('click', whenClick, false );
+        cell[i].addEventListener('click', afterClick, false );
 
     }
 
-    function whenClick(squer) {
-        when(squer.target.id, firstPlayer);
+    
+
+    function afterClick(squer) {
+        if (typeof orgBoard[squer.target.id] == 'number') {  
+        after(squer.target.id, firstPlayer);
+        if(!checkTie()) after(bestSpot(), secondPlayer);
+        } 
+        
     }
 
-    function when(squerId, player) {
-        orgBord[squerId] = player;
+    function after(squerId, player) {
+        orgBoard[squerId] = player;
         document.getElementById(squerId).innerText = player;
-        let gameWon = checkWin(orgBord, player);
+        let gameWon = checkWin(orgBoard, player);
         if(gameWon) gameOver(gameWon)
     }
 
@@ -53,11 +65,36 @@ function startGame() {
     function gameOver(gameWon) {
         for(let index of winSet[gameWon.index]) {
             document.getElementById(index).style.backgroundColor =
-            gameWon.player == firstPlayer ? "green" : "red";
+            gameWon.player == firstPlayer ? "green" : "blue";
         }
         for( var i = 0; i < cell.length; i++ ) {
-            cell[i].removeEventListener('click', whenClick, false );
+            cell[i].removeEventListener('click', afterClick, false );
         }
+        declareWinner(gameWon.player == firstPlayer ? "Wygrana!" : "Przegrana! Spróbuj jeszcze raz.")
+    }
+
+    function declareWinner(who) {
+        document.querySelector(".finish").style.display = "block";
+        document.querySelector(".finish .messege").innerText = who;
+    }
+
+    function emptySquer() {
+        return orgBoard.filter(s => typeof s == 'number');
+    }
+    function bestSpot() {
+        return emptySquer()[0];
+    }
+    
+    function checkTie() {
+        if (emptySquer().length == 0) {
+            for (var i = 0; i < cell.length; i++) {
+                cell[i].style.backgroundColor = "green";
+                cell[i].removeEventListener('click', afterClick, false);
+            }
+            declareWinner("Remis!")
+            return true;
+        }
+        return false;
     }
 
 }
